c85ba1edbd2c88b5c4ec6ada32b93ac556191a08
Brian Mc Conway <77050799+BrianMcConway@users.noreply.github.com>
2024-02-21 13:11:17 +0000

    Delete and upload new .png images, style for images. Add images in correct order wit JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+var rows = 3;
+var columns = 3;
+
+var picTile; //picture tiles
+var blankTile; //blank tile
+
+var turns = 0;
+
+window.onload = function () {
+    var imgOrder = ["assets/images/1.png", "assets/images/2.png", "assets/images/3.png", "assets/images/4.png", "assets/images/5.png", "assets/images/6.png", "assets/images/7.png", "assets/images/8.png", "assets/images/9.png"];
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = imgOrder.shift();
+
+
+            document.getElementById("puzzle-board").append(tile);
+
+        }
+    }
+}
+
