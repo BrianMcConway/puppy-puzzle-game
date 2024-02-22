e57a7225ba8a77b0fe6fb1803805a0c362196c94
Brian Mc Conway <77050799+BrianMcConway@users.noreply.github.com>
2024-02-22 10:33:29 +0000

    Rearrange buttons/controls html and add styling and responsiveness to same for small screens with media queries.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-var rows = 3;
+var rows = 3; //3x3 puzzle-board grid setup
 var columns = 3;
 
 var picTile; //picture tiles
 var blankTile; //blank tile
 
 var turns = 0;
-
+//add images to puzzle-board
 window.onload = function () {
     var imgOrder = ["assets/images/1.png", "assets/images/2.png", "assets/images/3.png", "assets/images/4.png", "assets/images/5.png", "assets/images/6.png", "assets/images/7.png", "assets/images/8.png", "assets/images/9.png"];
 
