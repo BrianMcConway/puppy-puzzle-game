11650e6698bda9d6a54f812026d3e4d1f41beb36
Brian Mc Conway <77050799+BrianMcConway@users.noreply.github.com>
2024-02-28 18:54:54 +0000

    Add JavaScript comments.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
-let rows = 3;
+let rows = 3; /** */
 let columns = 3;
-let emptyTile = { row: 2, col: 2 };
-let imgOrder = ["assets/images/2.png", "assets/images/5.png", "assets/images/3.png", "assets/images/7.png",
+let emptyTile = { row: 2, col: 2 }; /**  */
+let imgOrder = ["assets/images/2.png", "assets/images/5.png", "assets/images/3.png", "assets/images/7.png", /** */
     "assets/images/6.png", "assets/images/8.png", "assets/images/9.png", "assets/images/1.png", "assets/images/4.png"];
 
-let movesCount = 0;
-let puzzleSolved = false; // Flag to track puzzle state
+let movesCount = 0; /** Variable for moves counter*/
+let puzzleSolved = false; /**  Flag to track if puzzle is solved or not */
 
-// Event handler that executes function when HTML is loaded
+/**  Event handler that executes function when HTML is loaded */
 window.onload = function () {
-    // Hide the win modal on page load
+    /** Hide the win modal on page load */
     document.getElementById("modal-win").style.display = "none";
 
-    setupPuzzleBoard();
+    setupPuzzleBoard(); /** Initialize the puzzle board */
     document.getElementById("new-game").addEventListener("click", confirmStartNewGame);
     document.getElementById("how-to-play").addEventListener("click", openModal);
 }
 
-function confirmStartNewGame() {
+function confirmStartNewGame() { /** Confirm 'New Game' modal */
     let confirmationModal = document.getElementById("modal-sure");
     confirmationModal.style.display = "block";
 
@@ -31,28 +31,28 @@ function confirmStartNewGame() {
     });
 }
 
-function startNewGame() {
+function startNewGame() {  /** */
     movesCount = 0;
     updateTurnsCount();
     shuffleImages();
     setupPuzzleBoard();
-    puzzleSolved = false; // Reset the puzzleSolved flag
+    puzzleSolved = false; /**  Reset the puzzleSolved flag */
 }
 
-function shuffleImages() {
+function shuffleImages() {  /** */
     for (let i = imgOrder.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [imgOrder[i], imgOrder[j]] = [imgOrder[j], imgOrder[i]];
     }
 }
 
-function createClickHandler(row, col) {
+function createClickHandler(row, col) {  /** */
     return function() {
         moveTile(row, col);
     };
 }
 
-function setupPuzzleBoard() {
+function setupPuzzleBoard() {   /** */
     document.getElementById("puzzle-board").innerHTML = "";
 
     for (let r = 0; r < rows; r++) {
@@ -73,29 +73,29 @@ function setupPuzzleBoard() {
     }
 }
 
-function moveTile(row, col) {
+function moveTile(row, col) {    /** */
     if (isAdjacent(row, col, emptyTile.row, emptyTile.col)) {
         swapTiles(row, col, emptyTile.row, emptyTile.col);
         emptyTile = { row: row, col: col };
         movesCount++;
         updateTurnsCount();
 
-        if (isPuzzleSolved()) {
+        if (isPuzzleSolved()) {    /** */
             document.getElementById("turns").innerText = movesCount;
             showWinModal();
         }
     }
 }
 
-function updateTurnsCount() {
+function updateTurnsCount() {   /** */
     document.getElementById("turns").innerText = movesCount;
 }
 
-function isAdjacent(row1, col1, row2, col2) {
+function isAdjacent(row1, col1, row2, col2) {  /** */
     return (Math.abs(row1 - row2) === 1 && col1 === col2) || (Math.abs(col1 - col2) === 1 && row1 === row2);
 }
 
-function swapTiles(row1, col1, row2, col2) {
+function swapTiles(row1, col1, row2, col2) {  /** */
     let tempSrc = imgOrder[row1 * columns + col1];
     imgOrder[row1 * columns + col1] = imgOrder[row2 * columns + col2];
     imgOrder[row2 * columns + col2] = tempSrc;
@@ -104,7 +104,7 @@ function swapTiles(row1, col1, row2, col2) {
     document.getElementById(row2 + "-" + col2).src = imgOrder[row2 * columns + col2];
 }
 
-function isPuzzleSolved() {
+function isPuzzleSolved() {   /** */
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             if (imgOrder[r * columns + c] !== "assets/images/" + (r * columns + c + 1) + ".png") {
@@ -112,21 +112,21 @@ function isPuzzleSolved() {
             }
         }
     }
-    puzzleSolved = true; // Set the flag to true when the puzzle is solved
+    puzzleSolved = true; /**  Set the flag to true when the puzzle is solved */
     return true;
 }
 
-function openModal() {
+function openModal() {   /** */
     let modal = document.getElementById("modal-rules");
     modal.style.display = "block";
 }
 
-function closeModal() {
+function closeModal() {  /** */
     let modal = document.getElementById("modal-rules");
     modal.style.display = "none";
 }
 
-function showWinModal() {
+function showWinModal() {  /** */
 
     if (puzzleSolved) {
         let winModal = document.getElementById("modal-win");
@@ -144,21 +144,21 @@ function showWinModal() {
     }
 }
 
-document.getElementById("close-button").addEventListener("click", closeModal);
+document.getElementById("close-button").addEventListener("click", closeModal); /** */
 
 document.getElementById("close-win-button").addEventListener("click", function () {
     let winModal = document.getElementById("modal-win");
     winModal.style.display = "none";
 });
 
-let audio = document.getElementById("audio-player");
+let audio = document.getElementById("audio-player"); /** */
 let audioIcon = document.getElementById("audio-icon");
 
-audioIcon.addEventListener("click", function() {
+audioIcon.addEventListener("click", function() {  /** */
     toggleAudio();
 });
 
-function toggleAudio() {
+function toggleAudio() { /** */
     if (audio.paused) {
         audio.play();
         audioIcon.src = "assets/images/vol-on.png";
